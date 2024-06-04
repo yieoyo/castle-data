6a852e770312ca5c8f34094fbe8ff9584a49af52
yieoyo <66795032+yieoyo@users.noreply.github.com>
2024-06-04 23:07:33 +0600

    updated but not working

--- app/Http/Controllers/ScrapController.php
+++ app/Http/Controllers/ScrapController.php
@@ -20,8 +20,6 @@ class ScrapController extends Controller
         return view('scrape');
     }
 
-
-
     /**
      * Show the application dashboard.
      *
@@ -30,8 +28,6 @@ class ScrapController extends Controller
     // Handle the file upload
     public function upload(Request $request)
     {
-        $urla = 'https://www.argentina.gob.ar/justicia/registro-nacional-sociedades?cuit=';
-        $urlb = 'https://www.cuitonline.com/detalle/';
         // Validate the uploaded file
         $request->validate([
             'file' => 'required|mimes:txt',
@@ -42,138 +38,142 @@ class ScrapController extends Controller
             $file = $request->file('file');
             $fileName = 'data.txt';
             $file->move(public_path('uploads'), $fileName);
-
-            // You can process the file content here if needed
-
-            $filePath = public_path('uploads') . '/' . $fileName;
-            $fileContent = '';
-            if ($handle = fopen($filePath, 'r')) {
-                while (($line = fgets($handle)) !== false) {
-                    $cuid = substr($line, 0, 11);
-                    $url1 = $urla.$cuid;
-                    $url2 = $urlb.$cuid.'/';
-                    $dom1 = new \DOMDocument();
-                    $html1 = file_get_contents($url1);
-                    @ $dom1->loadHtml($html1);
-                    $v = $dom1->getElementById('show-data');
-                    $strongs = $v->getElementsByTagName('strong');
-                    $razonsocial = '';
-                    $direccion = '';
-                    $provincia = '';
-                    $localidad = '';
-                    $date = '';
-                    $priority = '';
-                    $fiscal_id = '';
-                    $description = '';
-                    foreach($strongs as $strong){
-                        if($strong->textContent == "Razón social"){
-                            $razonsocial = $strong->nextSibling->nextSibling->textContent;
-                        }
-                        if($strong->textContent == "Domicilio"){
-                            $direccion = $strong->nextSibling->nextSibling->textContent;
-                        }
-                        if($strong->textContent == "Provincia"){
-                            $provincia = $strong->nextSibling->nextSibling->textContent;
-                        }
-                        if($strong->textContent == "Localidad"){
-                            $localidad = $strong->nextSibling->nextSibling->textContent;
-                        }
-                    }
-                 
-                    $dom2 = new \DOMDocument();
-                    $html2 = @file_get_contents($url2);
-                    if ($html2 !== false) {
-                        @$dom2->loadHtml($html2);
-                        $uls = $dom2->getElementsByTagName('ul');
-                        $pattern = '/\[(\d{2}\/\d{4})\]\s+(\d+)\s+-\s+(.*)/';
-                        foreach($uls as $ul){
-                            if($ul->getAttribute('class') == 'p_info'){
-                                $li = $ul->lastChild->previousSibling->previousSibling->previousSibling->textContent;
-                                $parts = preg_split('/#\d+/', $li, -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE);                                 // Define the regular expression pattern
-                                array_shift($parts);
-
-                                // Reset array indices
-                                $parts = array_values($parts);
-
-                                foreach($parts as $key => $part){
-                                       
-                                    // Perform the regular expression match
-                                    if (preg_match($pattern, $part, $matches)) {
-                                        // Extract the desired parts
-                                        $priority =  $key +1 ;
-                                        $date = $matches[1];
-                                        $fiscal_id = $matches[2];
-                                        $description = $matches[3];
-                                    }
-                                }
-                                break;
-                            }
-                        }
-                        sleep(1);
-                    }
-                    // company
-                    $company = Company::where('cuit', $cuid)->first();
-                    if ($company){
-                        $company->update([
-                            'cuit' => $cuid,
-                            'razon_social' => $razonsocial,
-                            'direccion' => $direccion,
-                            'provincia' => $provincia,
-                            'localidad' => $localidad,
-                        ]);
-                    }else {
-                        $company = Company::create([
-                            'cuit' => $cuid,
-                            'razon_social' => $razonsocial,
-                            'direccion' => $direccion,
-                            'provincia' => $provincia,
-                            'localidad' => $localidad,
-                        ]);
-                    }
-
-                    // fiscal
-                    $fiscal = Fiscal::where('fiscal_id', $fiscal_id)->first();
-                    if ($fiscal){
-                        $fiscal->update([
-                            'fiscal_id' => $fiscal_id,
-                            'description' => $description,
-                        ]);
-                    }else {
-                        $fiscal = Fiscal::create([
-                            'fiscal_id' => $fiscal_id,
-                            'description' => $description,
-                        ]);
-                    }
-
-                    // activity
-                    if($company && $fiscal){
-                        $activity = Activity::where('company_id', $company->id)->where('id_actividad', $fiscal->id)->first();
-                        if($activity){
-                            $activity->update([
-                                'company_id' => $company->id,
-                                'id_actividad' => $fiscal->id,
-                                'prioridad' => $priority,
-                                'date' => $date,
-                            ]);
-                        }else {
-                            Activity::create([
-                                'company_id' => $company->id,
-                                'id_actividad' => $fiscal->id,
-                                'prioridad' => $priority,
-                                'date' => $date,
-                            ]);
-                        }
-
-
-                    }
-                }
-                fclose($handle);
-                dd('ok');
-            } else {
-                return redirect()->back()->withErrors(['file' => 'Error reading the file.']);
-            }
+            $this->scrape();
+            return redirect()->back()->with('success','File upload successfully.');
         }
 
-        return redirect()->back()->withErrors(['file' => 'File upload failed.']);
+        return redirect()->back()->with('error','File upload failed.');
+    }
+
+    function scrape(){   
+        $urla = 'https://www.argentina.gob.ar/justicia/registro-nacional-sociedades?cuit=';
+        $urlb = 'https://www.cuitonline.com/detalle/';
+        $filePath = public_path('uploads') . '/' . 'data.txt';
+        $fileContent = '';
+        if ($handle = fopen($filePath, 'r')) {
+            while (($line = fgets($handle)) !== false) {
+                $cuid = substr($line, 0, 11);
+                $url1 = $urla.$cuid;
+                $url2 = $urlb.$cuid.'/';
+                $dom1 = new \DOMDocument();
+                $html1 = file_get_contents($url1);
+                @ $dom1->loadHtml($html1);
+                $v = $dom1->getElementById('show-data');
+                $strongs = $v->getElementsByTagName('strong');
+                $razonsocial = '';
+                $direccion = '';
+                $provincia = '';
+                $localidad = '';
+                $date = '';
+                $priority = '';
+                $fiscal_id = '';
+                $description = '';
+                foreach($strongs as $strong){
+                    if($strong->textContent == "Razón social"){
+                        $razonsocial = $strong->nextSibling->nextSibling->textContent;
+                    }
+                    if($strong->textContent == "Domicilio"){
+                        $direccion = $strong->nextSibling->nextSibling->textContent;
+                    }
+                    if($strong->textContent == "Provincia"){
+                        $provincia = $strong->nextSibling->nextSibling->textContent;
+                    }
+                    if($strong->textContent == "Localidad"){
+                        $localidad = $strong->nextSibling->nextSibling->textContent;
+                    }
+                }
+             
+                $dom2 = new \DOMDocument();
+                $html2 = @file_get_contents($url2);
+                if ($html2 !== false) {
+                    @$dom2->loadHtml($html2);
+                    $uls = $dom2->getElementsByTagName('ul');
+                    $pattern = '/\[(\d{2}\/\d{4})\]\s+(\d+)\s+-\s+(.*)/';
+                    foreach($uls as $ul){
+                        if($ul->getAttribute('class') == 'p_info'){
+                            $li = $ul->lastChild->previousSibling->previousSibling->previousSibling->textContent;
+                            $parts = preg_split('/#\d+/', $li, -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE);                                 // Define the regular expression pattern
+                            array_shift($parts);
+
+                            // Reset array indices
+                            $parts = array_values($parts);
+
+                            foreach($parts as $key => $part){
+                                   
+                                // Perform the regular expression match
+                                if (preg_match($pattern, $part, $matches)) {
+                                    // Extract the desired parts
+                                    $priority =  $key +1 ;
+                                    $date = $matches[1];
+                                    $fiscal_id = $matches[2];
+                                    $description = $matches[3];
+                                }
+                            }
+                            break;
+                        }
+                    }
+                    sleep(1);
+                }
+                // company
+                $company = Company::where('cuit', $cuid)->first();
+                if ($company){
+                    $company->update([
+                        'cuit' => $cuid,
+                        'razon_social' => $razonsocial,
+                        'direccion' => $direccion,
+                        'provincia' => $provincia,
+                        'localidad' => $localidad,
+                    ]);
+                }else {
+                    $company = Company::create([
+                        'cuit' => $cuid,
+                        'razon_social' => $razonsocial,
+                        'direccion' => $direccion,
+                        'provincia' => $provincia,
+                        'localidad' => $localidad,
+                    ]);
+                }
+
+                // fiscal
+                $fiscal = Fiscal::where('fiscal_id', $fiscal_id)->first();
+                if ($fiscal){
+                    $fiscal->update([
+                        'fiscal_id' => $fiscal_id,
+                        'description' => $description,
+                    ]);
+                }else {
+                    $fiscal = Fiscal::create([
+                        'fiscal_id' => $fiscal_id,
+                        'description' => $description,
+                    ]);
+                }
+
+                // activity
+                if($company && $fiscal){
+                    $activity = Activity::where('company_id', $company->id)->where('id_actividad', $fiscal->id)->first();
+                    if($activity){
+                        $activity->update([
+                            'company_id' => $company->id,
+                            'id_actividad' => $fiscal->id,
+                            'prioridad' => $priority,
+                            'date' => $date,
+                        ]);
+                    }else {
+                        Activity::create([
+                            'company_id' => $company->id,
+                            'id_actividad' => $fiscal->id,
+                            'prioridad' => $priority,
+                            'date' => $date,
+                        ]);
+                    }
+
+
+                }
+            }
+            fclose($handle);
+            dd('ok');
+        } else {
+            return false;
+        }
     }
 }
